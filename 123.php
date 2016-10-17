ae514a6fe81e5c60d4cce2df2b71a3cd46331eb8
jiangxiaolin1995 <jiangxiaolin1995@gmail.com>
2016-10-17 21:26:19 +0800

    1

---
+++ 123.php
@@ -0,0 +1,26 @@
+<?php
+    // if ($_GET['url'] == 'baidu') {
+    //     echo "百度";
+    //     # code...
+    // }else {
+    //     echo "其他网站";
+    // }
+
+
+    if ($_POST['url'] == 'baidu') {
+        echo "百度";
+        # code...
+    }else {
+        echo "其他网站";
+    }
+
+
+
+
+
+
+
+
+
+
+ ?>
